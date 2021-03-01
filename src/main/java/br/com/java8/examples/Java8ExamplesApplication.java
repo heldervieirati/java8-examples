5ba20f5b4292ffd15786b61510300ccdb2db7a75
Helder Vieira <heldervieira_datum@getnet.com.br>
2021-03-01 13:54:23 -0300

    Criação do Projeto para Exemplos de Novas Funcionalidades do Java 8.

---
+++ src/main/java/br/com/java8/examples/Java8ExamplesApplication.java
@@ -0,0 +1,13 @@
+package br.com.java8.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8ExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8ExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/java8/examples/Java8ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java8.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8ExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
