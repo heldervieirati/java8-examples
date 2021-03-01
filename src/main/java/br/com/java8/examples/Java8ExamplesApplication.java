586fa5ad52e4ed3c9b86258513e6d94e7fae0f6f
Helder Vieira <heldervieira_datum@getnet.com.br>
2021-03-01 14:46:35 -0300

    Revert "Implementacao do Exemplo 1 para execução da classe Runnable sem e com a Expressao Lambda."
    This reverts commit 05a3196386403490d0afed6e9339011a2c31915a.

--- src/main/java/br/com/java8/examples/Java8ExamplesApplication.java
+++ src/main/java/br/com/java8/examples/Java8ExamplesApplication.java
@@ -8,50 +8,6 @@ public class Java8ExamplesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Java8ExamplesApplication.class, args);
-		System.out.println("******** Aplicação Executando... ********");
-		System.out.println();
-		
-		System.out.println("Example 1:");
-		example1();
-		
-		System.out.println();
-		System.out.println("Example 2:");
-		example2();
-		
-		System.out.println();
-		System.out.println("******** Aplicação Finalizada. ********");
 	}
 
-	private static void example1() {
-		//Implementação da classe anonima Runnable
-		Runnable r1 = new Runnable() {
-			
-			@Override
-			public void run() {
-				System.out.println("Estutando a expressao lambda 1.");
-				
-			}
-		};
-		
-		//Implementação da classe anonima Runnable com Lambda
-		Runnable r2;
-		r2 = () //Lista de argumentos 
-			 -> //Seta
-		     System.out.println("Estutando a expressao lambda 2."); //Corpo da Expressão Lambda
-		
-		r1.run();
-		r2.run();
-	}
-	
-	interface Num {
-		double getValue();
-	}
-	
-	private static void example2() {
-		
-		Num n;
-		n = () -> 16.18;
-		System.out.println(n.getValue());
-	}
-	
 }
